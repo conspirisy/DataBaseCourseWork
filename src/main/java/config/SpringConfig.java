87804002b0cef6b7839b68a58e6d486e5fe1cbb6
Dmitriy D. Kalinin <ddkalinin.work@gmail.com>
2018-06-12 18:38:36 +0800

    f

--- src/main/java/config/SpringConfig.java
+++ src/main/java/config/SpringConfig.java
@@ -1,10 +1,11 @@
 package config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * Created by INSIGHT on 11.06.2018.
  */
-@Configuration
+
 public class SpringConfig {
 }
--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,7 +1,24 @@
 package controller;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  * Created by INSIGHT on 11.06.2018.
  */
-public interface UserController {
+@Controller
+@RequestMapping("/")
+@ComponentScan(basePackages = "controller")
+public class UserController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 }
